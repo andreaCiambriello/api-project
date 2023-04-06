a2f40bfd27a98fe83e03447ffea803c7e52458ad
Andrea Ciambriello <117306401+andreaCiambriello@users.noreply.github.com>
2023-04-06 16:56:42 +0200

    Move all configuration into one script

--- src/server.ts
+++ src/server.ts
@@ -1,22 +1,11 @@
 import "dotenv/config";
 
-// import express from "express";
-// import "express-async-errors";
-
-// const app = express();
-
-// app.get("/", (request, response) => {
-//     response.send("Up and running!");
-// });
+import config from "./config";
 
 import app from "./app";
 
-// const port = 3000;
-
-const port = process.env.PORT;
+const port = config.PORT;
 
 app.listen(port, () => {
     console.log(`[server]: Server is running at http://localhost:${port}`);
 })
-
-//commento perché ho diviso le due parti in due file, per avere tutto qui basta scommentare, togliere l'import e il server funziona lo stesso
