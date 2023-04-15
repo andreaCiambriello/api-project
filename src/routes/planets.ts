82095237758d62539b13c97474b6c33aa4a398fd
Andrea Ciambriello <117306401+andreaCiambriello@users.noreply.github.com>
2023-04-15 11:07:52 +0200

    Configure protected routes

--- src/routes/planets.ts
+++ src/routes/planets.ts
@@ -1,6 +1,7 @@
 import express, { Router } from "express";
 import prisma from "../lib/prisma/client";
 import { validate, planetSchema, PlanetData } from "../lib/middleware/validation";
+import { checkAuthorization } from "../lib/middleware/passport";
 import { initMulterMiddleware } from "../lib/middleware/multer";
 
 const upload = initMulterMiddleware();
@@ -28,7 +29,7 @@ router.get("/:id(\\d+)", async (request, response, next) => {
     response.json(planet);
 });
 
-router.post("/", validate({ body: planetSchema }), async (request, response) => {
+router.post("/", checkAuthorization, validate({ body: planetSchema }), async (request, response) => {
     const planetData: PlanetData = request.body;
 
     const planet = await prisma.planet.create({
@@ -38,7 +39,7 @@ router.post("/", validate({ body: planetSchema }), async (request, response) => 
     response.status(201).json(planet);
 });
 
-router.put("/:id(\\d+)", validate({ body: planetSchema }), async (request, response, next) => {
+router.put("/:id(\\d+)", checkAuthorization, validate({ body: planetSchema }), async (request, response, next) => {
     const planetId = Number(request.params.id);
     const planetData: PlanetData = request.body;
 
@@ -55,7 +56,7 @@ router.put("/:id(\\d+)", validate({ body: planetSchema }), async (request, respo
     }
 });
 
-router.delete("/:id(\\d+)", async (request, response, next) => {
+router.delete("/:id(\\d+)", checkAuthorization, async (request, response, next) => {
     const planetId = Number(request.params.id);
 
     try {
@@ -70,7 +71,7 @@ router.delete("/:id(\\d+)", async (request, response, next) => {
     }
 });
 
-router.post("/:id(\\d+)/photo", upload.single("photo"), async (request, response, next) => {
+router.post("/:id(\\d+)/photo", checkAuthorization, upload.single("photo"), async (request, response, next) => {
     if (!request.file) {
         response.status(400);
         return next("No photo file uploaded");
